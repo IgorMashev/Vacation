19809a6e70d47ab7d3a57ea63020595082be072c
unicom510 <o.tarasiuk@internetdevels.com>
2018-09-18 18:13:16 +0300

    last commit day 18.09

---
+++ themes/custom/bootstrap_sass/js/script2.js
@@ -0,0 +1,47 @@
+(function ($) {
+  
+    Drupal.behaviors.daysCounter = {
+        attach: function(context, settings) {
+          var $webForm = $('.webform-submission-request-form');
+          var $startDate = $('input[data-drupal-selector="edit-from-date"]', $webForm);
+          var $endDate = $('input[data-drupal-selector="edit-to-date"]', $webForm);
+          var $daysCount = $('input[data-drupal-selector="edit-order"]');
+
+          var startDate = null;
+          var endDate = null;
+
+          $($startDate).once().change(function () {
+            startDate = $(this).val();
+
+            if (startDate && endDate) {
+              $daysCount.val(countDays(startDate, endDate));
+            }
+
+          });
+
+          $($endDate).once().change(function () {
+            endDate = $(this).val();
+            if (startDate && endDate) {
+              $daysCount.val(countDays(startDate, endDate));
+            }
+          });
+
+          function countDays(start, end) {
+            var startDate = convertDate(start);
+            var endDate = convertDate(end);
+            var seconds = endDate - startDate;
+
+            return seconds/86400000;
+          }
+
+          function convertDate(date) {
+            var dateArray = date.split("-");
+            var converted = dateArray[0]+"/"+dateArray[1]+"/"+dateArray[2];
+            var timestamp = new Date(converted);
+
+            return timestamp.getTime();
+          }
+        }
+    };
+
+  }(jQuery));
